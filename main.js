bd30583cfdbdc8f9bd68c5d49fcd60112f1091fe
Yeh-Yung <yyc1217@gmail.com>
2016-04-22 16:53:18 +0800

    Move marker while audio is playing

--- main.js
+++ main.js
@@ -16,54 +16,92 @@ Array.prototype.centroid = function () {
     };
 };
 
-Array.prototype.lerp = function(lerpStep) {
+Array.prototype.lerp = function (lerpStep) {
     var self = this;
 
-	var result = [];
+    var result = [];
 
+    self.forEach(function (elem) {
+        elem.time = moment(elem.time);
+    });
 
-	self.forEach(function (elem) {
-		elem.time = moment(elem.time);
-	});
+    /**
+    from point1 ~ (point2 - 1)
+     */
+    var lerpBetween = function (point1, point2) {
 
-	/**
-	from point1 ~ (point2 - 1)
-	 */
-	var lerpBetween = function (point1, point2) {
-        
         var lerp = function (v0, v1, t) {
             return (1 - t) * v0 + t * v1; ;
         };
-		var diff = point2.time.diff(point1.time);
-		var numOfInterpolatePoint = Math.floor(diff / lerpStep);
+        var diff = point2.time.diff(point1.time);
+        var numOfInterpolatePoint = Math.floor(diff / lerpStep);
 
-		var lerped = [];
-		for (var j = 0; j < numOfInterpolatePoint; j++) {
-			var t = j / numOfInterpolatePoint;
-			var lerpLat = lerp(point1.lat, point2.lat, t);
-			var lerpLng = lerp(point1.lng, point2.lng, t);
-			lerped.push({
-				"lat" : lerpLat,
-				"lng" : lerpLng
-			});
-		}
-		return lerped;
-	};
+        var lerped = [];
+        for (var j = 0; j < numOfInterpolatePoint; j++) {
+            var t = j / numOfInterpolatePoint;
+            var lerpLat = lerp(point1.lat, point2.lat, t);
+            var lerpLng = lerp(point1.lng, point2.lng, t);
+            lerped.push({
+                "lat" : lerpLat,
+                "lng" : lerpLng
+            });
+        }
+        return lerped;
+    };
 
-	var expectedCount = Math.floor(self[self.length - 1].time.diff(self[0].time) / lerpStep);
-	console.log("應該要有", expectedCount, "個點");
+    var expectedCount = Math.floor(self[self.length - 1].time.diff(self[0].time) / lerpStep);
+    console.log("Should have ", expectedCount, " points.");
 
-	for (var i = 0; i < self.length - 1; i++) {
-		var lerpedPoints = lerpBetween(self[i], self[i + 1]);
-		Array.prototype.push.apply(result, lerpedPoints);
-	}
+    for (var i = 0; i < self.length - 1; i++) {
+        var lerpedPoints = lerpBetween(self[i], self[i + 1]);
+        Array.prototype.push.apply(result, lerpedPoints);
+    }
 
     return result;
 
 };
-var lerpStep = 1000;
-var result = data.path.lerp(lerpStep);
-result.forEach(function (elem) {
-    console.log(elem.lat, ", ", elem.lng);
-});
-//console.log(data.path.centroid());
+
+MapTapePlayer = function(map, path, audio) {
+    
+    var lerpStep = 1000;
+    var points = path.lerp(lerpStep);
+    var map = map;
+    var audio = audio;
+    
+    var pointer = new google.maps.Marker({
+        position : points[0],
+        map: map
+    });
+    
+    audio.ontimeupdate = function() {
+        var index = Math.floor(audio.currentTime);
+        
+        if (index < points.length) {
+            pointer.setPosition(points[index]);
+        }
+    };
+    
+    return {
+        play: function() {
+            audio.play();
+        },
+        
+        pause: function() {
+            audio.pause();
+        }
+    };
+    
+};
+
+var player;
+
+function init() {
+
+    var center = data.path.centroid();
+    var map = new google.maps.Map(document.getElementById('map'), {
+            center : center,
+            zoom : data.zoom_level
+    });
+    
+    player = new MapTapePlayer(map, data.path, document.getElementsByTagName('audio')[0]);
+};
